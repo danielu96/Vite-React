366ff316cd2442e6b47a927121460cd4df12bf38
Dniel K <danielu96@wp.pl>
2023-03-29 00:05:21 +0200

    data (name,title) change

--- src/data.jsx
+++ src/data.jsx
@@ -3,8 +3,8 @@ const people = [
       id: 1,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
-      name: 'maria ferguson',
-      title: 'office manager',
+      name: 'Kubek',
+      title: 'naduk kolorowy',
       quote:
         'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
     },
@@ -12,8 +12,8 @@ const people = [
       id: 2,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
-      name: 'john doe',
-      title: 'regular guy',
+      name: 'Kufel',
+      title: 'nadruk czarny',
       quote:
         'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
     },
@@ -21,8 +21,8 @@ const people = [
       id: 3,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
-      name: 'peter smith',
-      title: 'product designer',
+      name: 'Wizytówka metalowa',
+      title: 'nadruk klorowy',
       quote:
         'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
     },
@@ -30,8 +30,8 @@ const people = [
       id: 4,
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
-      name: 'susan andersen',
-      title: 'the boss',
+      name: 'krysztal',
+      title: 'nadruk kolorowy',
       quote:
         'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
     },
