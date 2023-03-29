2249b12297a53d287df4981d4f0028af77c109ef
Dniel K <danielu96@wp.pl>
2023-03-29 13:47:35 +0200

    empty space deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { useState } from 'react'
 import Carousel from './Carousel'
-
 import './App.css'
-
 function App() {
-
-
   return (
-
     <Carousel/>
-    // <div className="App">
-      
-    //   <h1>Carousel</h1>
-     
-     
-
+    // <div className="App">      
     // </div>
   )
 }
-
 export default App
